bf6bfc3ccc82822d9eacd36260a387c3681aa9a4
Hannah Lee <013hannah.a.lee@gmail.com>
2021-03-01 06:57:27 -0600

    added a basic layout in html, made a button that clicks

---
+++ Assets/script.js
@@ -0,0 +1,12 @@
+var quizButton = document.querySelector('#start');
+var highScore = [];
+
+function startQuiz() {
+    console.log("Quiz Started!");
+};
+
+
+quizButton.addEventListener("click", function () {
+    console.log("Clicked!");
+    startQuiz;
+});
