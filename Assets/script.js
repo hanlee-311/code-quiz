11d0ad341496b508334f6a8fcc553408aee2e210
Hannah Lee <013hannah.a.lee@gmail.com>
2021-03-01 08:49:05 -0600

    Added answer buttons in html, javascript can generate and display a question

--- Assets/script.js
+++ Assets/script.js
@@ -1,24 +1,34 @@
 var quizButton = document.querySelector('#start');
 var displayedQuestion = document.querySelector('.quiz-container');
 var hideStartScreen = document.querySelector('#start-up-screen');
+var questionElement = document.querySelector('.quiz-question');
+var answerElement = document.querySelector('.answer-button');
 var highScore = [];
-var quizQuestions = [{"question": "A very useful tool to help with debugging and printing content to the debugger is the: ",
-"choices": ["Console Log", "Terminal", "Ice Cream", "Javascript"], 
-"correct": "Console Log"
+var quizQuestions = [{question: "A very useful tool to help with debugging and printing content to the debugger is the: ",
+choices: ["Console Log", "Terminal", "Ice Cream", "Javascript"], 
+correct: "Console Log"
 }];
+let shuffleQuestions, currentQuestionIndex;
 
 function startQuiz() {
     console.log("Quiz Started!");
     hideStartScreen.classList.add('hide');
     displayedQuestion.classList.remove('hide');
-    generateQuestion();
+    shuffleQuestions = quizQuestions.sort(() => Math.random () - .5);
+    currentQuestionIndex = 0;
+    nextQuestion();
 };
 
-function generateQuestion() {
-    console.log("Generating Question");
-    
+function nextQuestion() {
+    console.log("Next Question");
+    generateQuestion (shuffleQuestions[currentQuestionIndex]);
     
 };
+
+function generateQuestion (question) {
+    console.log("Generating Question");
+    questionElement.innerHTML = question.question;
+}
 
 //Start the quiz by pressing the button
 quizButton.addEventListener("click",  startQuiz)
