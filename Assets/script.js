a62a34536186c2f8c7f84c34e9aab1c981fe31bf
Hannah Lee <013hannah.a.lee@gmail.com>
2021-03-01 11:59:33 -0600

    added another question, checked that it could randomize the order of questions

--- Assets/script.js
+++ Assets/script.js
@@ -4,12 +4,9 @@ var hideStartScreen = document.querySelector('#start-up-screen');
 var questionElement = document.querySelector('.quiz-question');
 var answerElement = document.querySelector('.answer-button');
 var highScore = [];
-var quizQuestions = [{question: "A very useful tool to help with debugging and printing content to the debugger is the: ",
-choices: [{text: "Console Log"}, {text: "Terminal"}, {text: "Ice Cream"}, {text: "Javascript"}], 
-correct: "Console Log"
-}];
 let shuffleQuestions, currentQuestionIndex;
 
+//Populating the page with the quiz after starting and randomizes the order
 function startQuiz() {
     console.log("Quiz Started!");
     hideStartScreen.classList.add('hide');
@@ -19,25 +16,64 @@ function startQuiz() {
     nextQuestion();
 };
 
+//Set the next question
 function nextQuestion() {
     console.log("Next Question");
     generateQuestion (shuffleQuestions[currentQuestionIndex]);
     
 };
 
+//Takes a question from the question array and corresponding answers
 function generateQuestion (question) {
     console.log("Generating Question");
     questionElement.innerHTML = question.question;
     question.choices.forEach(answer => {
         const button = document.createElement('button'); 
         button.innerText = answer.text;
-    //     button.classList.add('btn');
-    //     if (answer.correct) {
-    //         button.dataset.correct = answer.correct
-    //     };
-    answerElement.appendChild(button);
+        button.classList.add('btn');
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        };
+        button.addEventListener('click', selectAnswer);
+        answerElement.appendChild(button);
     })
 }
 
-//Start the quiz by pressing the button
+function selectAnswer (e) {
+    const selectedButton = e.target;
+    const correctAnswer = selectedButton.dataset.correct;
+    setStatusClass(document.body, correctAnswer);
+    Array.from(answerElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correctAnswer);
+    })
+}
+
+function setStatusClass (element, correct) {
+    if (correct) {
+        element.classList.add('correct');
+        console.log('correct!');
+    } 
+}
+
+
+
+//Start the quiz by pressing the start button
 quizButton.addEventListener("click",  startQuiz)
+
+var quizQuestions = [
+    {question: "A very useful tool to help with debugging and printing content to the debugger is the:",
+    choices: [
+        {text: "Console Log", correct: true}, 
+        {text: "Terminal", correct: false}, 
+        {text: "Ice Cream", correct: false}, 
+        {text: "Javascript", correct: false}
+    ]},
+
+    {question: "Commonly used data types DO NOT include:",
+    choices: [
+        {text: "Boolean", correct: true}, 
+        {text: "Strings", correct: false}, 
+        {text: "Alerts", correct: false}, 
+        {text: "Numbers", correct: false}
+    ]}
+];
