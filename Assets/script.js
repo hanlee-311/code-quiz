0dffd678828926fbeba793da0b67bbea317afb1d
Hannah Lee <013hannah.a.lee@gmail.com>
2021-03-01 08:22:01 -0600

    added css sheet, added classes and ids so that pressing the start button causes the quiz questions to eventually appear and the start up screen to vanish

--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,24 @@
 var quizButton = document.querySelector('#start');
+var displayedQuestion = document.querySelector('.quiz-container');
+var hideStartScreen = document.querySelector('#start-up-screen');
 var highScore = [];
+var quizQuestions = [{"question": "A very useful tool to help with debugging and printing content to the debugger is the: ",
+"choices": ["Console Log", "Terminal", "Ice Cream", "Javascript"], 
+"correct": "Console Log"
+}];
 
 function startQuiz() {
     console.log("Quiz Started!");
+    hideStartScreen.classList.add('hide');
+    displayedQuestion.classList.remove('hide');
+    generateQuestion();
 };
 
+function generateQuestion() {
+    console.log("Generating Question");
+    
+    
+};
 
-quizButton.addEventListener("click", function () {
-    console.log("Clicked!");
-    startQuiz;
-});
+//Start the quiz by pressing the button
+quizButton.addEventListener("click",  startQuiz)
