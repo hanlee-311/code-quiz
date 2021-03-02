a975a05c65527b2815456a58e4c5cecc7ef63799
Hannah Lee <013hannah.a.lee@gmail.com>
2021-03-01 21:03:38 -0600

    updated timer so it stops on other pages, store score, fixed quiz so it goes through all questions

--- Assets/script.js
+++ Assets/script.js
@@ -5,14 +5,18 @@ var returnBtn = document.querySelector(".return-btn");
 var endScreen = document.querySelector('.end-screen'); 
 var viewHighScores = document.querySelector('.high-score');
 var highScoreContainer = document.querySelector('.high-score-container');
+var highScoreList = document.querySelector('.user-high-score');
 var highScoreScreen = document.querySelector('.restart-screen');
+var highScoreForm = document.querySelector('#input');
+var userInitials = document.querySelector('.user-input');
 var displayedQuestion = document.querySelector('.quiz-container');
 var hideStartScreen = document.querySelector('#start-up-screen');
 var questionElement = document.querySelector('.quiz-question');
 var answerElement = document.querySelector('.answer-button');
 var timeEl = document.querySelector(".time");
-var secondsLeft = 5;
-var highScore = [];
+var timeVariable;
+var secondsLeft = 50;
+var score = 0;
 let shuffleQuestions, currentQuestionIndex;
 
 //Populating the page with the quiz after starting and randomizes the order
@@ -40,37 +44,45 @@ function generateQuestion (question) {
         const button = document.createElement('button'); 
         button.innerText = answer.text;
         button.classList.add('btn-dark', 'btn');
-        if (answer.correct) {
+        // if (answer.correct) {
             button.dataset.correct = answer.correct
-        };
-        button.addEventListener('click', selectAnswer);
+        // };
+
+        button.addEventListener('click', guessAnswer);
         answerElement.appendChild(button);
     })
 }
 
-function selectAnswer (e) {
+//Action upon selecting an answer
+function guessAnswer (e) {
+    console.log('Answer selected');
+    // get the button from the event
     const selectedButton = e.target;
+    console.log(e.target);
+    // pull out the boolean represntinv if its true or not
     const correctAnswer = selectedButton.dataset.correct;
-    setStatusClass(document.body, correctAnswer);
-    Array.from(answerElement.children).forEach(button => {
-        setStatusClass(button, button.dataset.correctAnswer);
-    })
-    if (shuffleQuestions.length > currentQuestionIndex) {
+    console.log(correctAnswer);
+    // check if its correct
+    checkIfCorrect(correctAnswer);
+    if (currentQuestionIndex < 4) {
         nextQuestion();
     } else {
         endQuiz();
     }
+   
 }
 
-function setStatusClass (element, correct) {
-    if (correct) {
-        element.classList.add('correct');
+function checkIfCorrect (correct) {
+    if (correct == 'true') {
+        // add to the score
+        score++;
         console.log('correct!');
-        currentQuestionIndex++;
-        nextQuestion();
     } else {
         console.log('wrong!');
+        //Subtract Time
+        secondsLeft = secondsLeft - 10;
     }
+    currentQuestionIndex++;
 }
 
 //Removes the previous question's buttons
@@ -86,13 +98,15 @@ function resetState () {
 function endQuiz () {
     endScreen.classList.remove('hide');
     displayedQuestion.classList.add('hide');
+    stopTimer();
 }
 
 //Populates page with highscore screen
-function restartScreen () {
+function restartScreen (event) {
+    event.preventDefault();
     highScoreScreen.classList.remove('hide');
     endScreen.classList.add('hide');
-
+    storeHighScores();
 }
 
 //Populates page with start screen
@@ -111,12 +125,28 @@ function allHighScores () {
     endScreen.classList.add('hide');
     highScoreContainer.classList.remove('hide');
     displayedQuestion.classList.add('hide');
+    stopTimer();
 }
+
+//Generating the highscore list
+var highScore = [{
+    initals: userInitials,
+    score: score,
+}];
+var userInitials = 
+
+function storeHighScores () {
+    
+    localStorage.setItem("highscore", JSON.stringify(highScore));
+    console.log("Stored " + score);
+};
+
+
 
 //Button actions
 
 //Start the quiz by pressing the start button
-quizButton.addEventListener("click",  startQuiz);
+quizButton.addEventListener("click", startQuiz);
 
 //Submit Initials button
 submitBtn.addEventListener('click', restartScreen);
@@ -133,16 +163,21 @@ returnBtn.addEventListener('click', startScreen);
 //Timer
 function setTime() {
     timeEl.classList.remove('hide');
-    var timerInterval = setInterval(function () {
+    timeVariable = setInterval(function () {
         secondsLeft--;
         timeEl.textContent = "Time: " + secondsLeft;
 
         if(secondsLeft ===0) {
-            clearInterval(timerInterval);
+            stopTimer();
             endQuiz();
-        }
+        } 
     }, 1000);
 }
+
+function stopTimer () {
+    clearInterval(timeVariable);
+    }
+
 
 var quizQuestions = [
     {question: "A very useful tool to help with debugging and printing content to the debugger is the:",
@@ -177,3 +212,5 @@ var quizQuestions = [
         {text: "4. Numbers", correct: false}
     ]}
 ];
+
+// init()
